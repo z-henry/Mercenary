6d96747143403497afaf60cc6d91eeb0e8720044
翰艺 郑 <henry_zzzzz@outlook.com>
2022-07-04 16:13:40 +0800

    红龙 一举两得 带玉龙 佣兵id录入

--- Mercenary/Const.cs
+++ Mercenary/Const.cs
@@ -6,80 +6,120 @@ namespace Mercenary
 	// Token: 0x02000005 RID: 5
 	internal class MercConst
 	{
-		// Token: 0x04000018 RID: 24
-		public const int VaLiLa = 17;
-
-		// Token: 0x04000019 RID: 25
-		public const int KaiRuiEr = 18;
-
-		// Token: 0x0400001A RID: 26
+		public const int 乌瑟尔 = 94;
+		public const int 亚煞极 = 344;
+		public const int 伊利丹 = 12;
+		public const int 伊瑞尔 = 367;
+		public const int 伊莉斯 = 383;
+		public const int 先知维伦 = 5;
+		public const int 光明之翼 = 24;
+		public const int 冰雪之王洛克霍拉 = 251;
+		public const int 凯恩 = 26;
+		public const int 凯瑞尔 = 18;
+		public const int 剑圣萨穆罗 = 53;
+		public const int 加尔范上尉 = 250;
+		public const int 加尔鲁什 = 50;
+		public const int 加拉克苏斯大王 = 2;
+		public const int 厨师曲奇 = 149;
+		public const int 变装大师 = 376;
+		public const int 古夫 = 28;
+		public const int 古尔丹 = 60;
+		public const int 吉安娜 = 97;
+		public const int 塔姆辛 = 45;
+		public const int 塔维什 = 44;
+		public const int 奈法利安 = 306;
+		public const int 奔波尔霸 = 312;
+		public const int 奥妮克希亚 = 291;
+		public const int 娜塔莉 = 8;
+		public const int 安东尼达斯 = 98;
+		public const int 安娜科德拉 = 63;
+		public const int 安度因 = 93;
+		public const int 尤朵拉 = 236;
+		public const int 尤格 = 308;
+		public const int 巫妖王 = 99;
+		public const int 巴琳达 = 249;
+		public const int 布莱恩 = 373;
+		public const int 布鲁坎 = 21;
+		public const int 希奈丝特拉 = 253;
+		public const int 希尔瓦娜斯 = 15;
+		public const int 库尔特鲁斯 = 3;
+		public const int 德雷克塔尔 = 293;
+		public const int 恩佐斯 = 341;
+		public const int 拉希奥 = 252;
+		public const int 拉格纳罗斯 = 20;
+		public const int 拉索利安 = 55;
+		public const int 指挥官沃恩 = 57;
+		public const int 提里奥 = 4;
+		public const int 斯卡布斯 = 7;
+		public const int 斯尼德 = 156;
+		public const int 暴龙王克鲁什 = 43;
+		public const int 格罗玛什 = 6;
+		public const int 格鲁尔 = 25;
+		public const int 梵妮莎 = 238;
+		public const int 死亡之翼 = 298;
+		public const int 沃金 = 62;
+		public const int 泰兰德 = 69;
+		public const int 泰瑞尔 = 311;
 		public const int 泽瑞拉 = 19;
-
-		// Token: 0x0400001B RID: 27
-		public static int LUO_SI = 20;
-
-		// Token: 0x0400001C RID: 28
-		public static int JIA_DUN = 22;
-
-		// Token: 0x0400001D RID: 29
-		public const int 晨拥 = 38;
-
-		// Token: 0x0400001E RID: 30
-		public static int TA_MU_XIN = 45;
-
-
-		public static int 玛诺洛斯 = 54;
-
-		// Token: 0x0400001F RID: 31
-		public static int LA_SUO_LI_AN = 55;
-
-		// Token: 0x04000020 RID: 32
-		public const int BinChiZhe = 71;
-
-		// Token: 0x04000021 RID: 33
-		public static int KAO_NEI_LIU_SI = 95;
-
-		// Token: 0x04000022 RID: 34
-		public static int AN_DONG_NI = 98;
-
-		// Token: 0x04000023 RID: 35
-		public static int SHAN_HU = 100;
-
-		// Token: 0x04000024 RID: 36
-		public static int QU_QI = 149;
-
-		public static int 尤朵拉 = 236;
-
-		// Token: 0x04000025 RID: 37
-		public const int BaLinDa = 249;
-
-		// Token: 0x04000026 RID: 38
-		public static int LUO_KE_HUO_LA = 251;
-
-		// Token: 0x04000027 RID: 39
-		public static int YU_LONG = 285;
-
-		// Token: 0x04000028 RID: 40
-		public static int CHI_JING = 286;
-
-		// Token: 0x04000029 RID: 41
-		public static int 帕奇斯 = 374;
+		public const int 洛卡拉 = 11;
+		public const int 海巫扎尔吉拉 = 294;
+		public const int 海盗帕奇斯 = 374;
+		public const int 深水领主卡拉瑟雷斯 = 295;
+		public const int 游学者周卓 = 366;
+		public const int 滑矛领主 = 305;
+		public const int 潮汐主母阿茜萨 = 304;
+		public const int 火车王里诺艾 = 284;
+		public const int 玉珑 = 285;
+		public const int 玛法里奥 = 64;
+		public const int 玛维 = 372;
+		public const int 玛诺洛斯 = 54;
+		public const int 珑心 = 254;
+		public const int 瓦丝琪女士 = 289;
+		public const int 瓦尔登 = 38;
+		public const int 瓦罗克 = 52;
+		public const int 瓦莉拉 = 17;
+		public const int 瓦里安 = 9;
+		public const int 砮皂 = 283;
+		public const int 神谕者摩戈尔 = 41;
+		public const int 穆克拉 = 42;
+		public const int 穆坦努斯 = 56;
+		public const int 空军上将罗杰斯 = 242;
+		public const int 米尔豪斯 = 14;
+		public const int 老瞎眼 = 40;
+		public const int 考内留斯 = 95;
+		public const int 艾德温 = 159;
+		public const int 艾萨拉女王 = 290;
+		public const int 范达尔 = 292;
+		public const int 萨尔 = 49;
+		public const int 贝恩 = 371;
+		public const int 赤精 = 286;
+		public const int 迦顿男爵 = 22;
+		public const int 迪亚波罗 = 102;
+		public const int 重拳先生 = 237;
+		public const int 钩牙船长 = 309;
+		public const int 闪狐 = 100;
+		public const int 阿莱克丝塔萨 = 23;
+		public const int 雪怒 = 282;
+		public const int 雷克萨 = 1;
+		public const int 雷诺 = 377;
+		public const int 鞭笞者特里高雷 = 71;
+		public const int 魔像师卡扎库斯 = 155;
 
 		// Token: 0x0400002A RID: 42
 		public static readonly List<int> First = new List<int>
 		{
-			MercConst.BaLinDa,
-			MercConst.VaLiLa,
-			MercConst.LUO_SI,
-			MercConst.QU_QI,
-			MercConst.BinChiZhe
+			MercConst.巴琳达,
+			MercConst.瓦莉拉,
+			MercConst.拉格纳罗斯,
+			MercConst.厨师曲奇,
+			MercConst.鞭笞者特里高雷
 		};
 
 		// Token: 0x0400002B RID: 43
 		public static readonly List<int> Ignore = new List<int>
 		{
-			MercConst.LUO_KE_HUO_LA,
-			MercConst.CHI_JING
+			MercConst.冰雪之王洛克霍拉,
+			MercConst.赤精
 		};
 	}
 
