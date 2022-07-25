864e6deede0f05eafd2ac9ed55356c4a32249679
hy z <henry_zzzzz@outlook.com>
2022-07-25 17:48:12 +0800

    版本号

--- Mercenary/Main.cs
+++ Mercenary/Main.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 
 namespace Mercenary
 {	
-	[BepInPlugin("io.github.jimowushuang.hs", "佣兵挂机插件[改]", "3.1.0")]
+	[BepInPlugin("io.github.jimowushuang.hs", "佣兵挂机插件[改]", "3.1.1")]
 	public class Main : BaseUnityPlugin
 	{
 		
@@ -25,7 +25,7 @@ namespace Mercenary
 			{
 				return;
 			}
-			GUILayout.Label(new GUIContent("3.1.0"), new GUILayoutOption[]
+			GUILayout.Label(new GUIContent("3.1.1"), new GUILayoutOption[]
 			{
 				GUILayout.Width(200f)
 			});
