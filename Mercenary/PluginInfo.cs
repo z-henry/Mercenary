8e56bb7ebe2cfb89ba82cb6485f3ee515a2578d3
hy z <henry_zzzzz@outlook.com>
2022-10-26 20:13:50 +0800

    4.1.4

--- Mercenary/PluginInfo.cs
+++ Mercenary/PluginInfo.cs
@@ -4,6 +4,6 @@
 	{
 		public const string PLUGIN_GUID = "io.github.jimowushuang.hs";
 		public const string PLUGIN_NAME = "佣兵挂机插件[改]";
-		public const string PLUGIN_VERSION = "4.1.3";
+		public const string PLUGIN_VERSION = "4.1.4";
 	}
 }
